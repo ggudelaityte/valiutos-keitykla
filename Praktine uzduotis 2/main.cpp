5d41d069c94d10b0bf783330e7d6907abaa804db
ggudelaityte <ggudelaityte@gmail.com>
2026-04-06 20:01:53 +0300

    trečias atnaujinimas

--- Praktine uzduotis 2/main.cpp
+++ Praktine uzduotis 2/main.cpp
@@ -35,11 +35,12 @@ int main() {
                 int kiek;
                 cout << "Kiek pazymiu (max " << MAX_PAZYMIU << ")? ";
                 cin >> kiek;
-            // Apsauga nuo netinkamo įvedimo
+                
+                // Apsauga nuo netinkamo įvedimo
                 if (kiek > MAX_PAZYMIU) kiek = MAX_PAZYMIU;
                 if (kiek < 0) kiek = 0; 
 
-                // Įvedamas kiekiena pažymys su apsauga 
+                // Įvedamas kiekvienas pažymys su apsauga 
                 for (int i = 0; i < kiek; i++) {
                     int laikinasPazymys;
                     do {
@@ -53,6 +54,7 @@ int main() {
                     // Tinkamas pažymys išsaugomas masyve
                     pazymiai[mokiniuSkaicius][i] = laikinasPazymys;
                 }
+                
                 // Išsaugoma kiek pažymių gavo mokinys ir padidinamas mokinių skaičius
                 pazymiuKiekis[mokiniuSkaicius] = kiek;
                 mokiniuSkaicius++;
@@ -91,22 +93,71 @@ int main() {
                 cout << "Klaida: Tokio mokinio nera.\n";
             }
         }
-
+        // Pažymio keitimas
         else if (pasirinkimas == 4) { 
             int mNr, pNr;
             cout << "Kurio mokinio pazymi keisti? ";
             cin >> mNr;
-            int mIdx = mNr - 1;
-            if (mIdx >= 0 && mIdx < mokiniuSkaicius) 
-            // patikrinimas kad įvesttas būtų galiojantis mokinio nr
-            {
+            int mIdx = mNr - 1; 
+            
+            // Jei mokinys yra sąraše
+            if (mIdx >= 0 && mIdx < mokiniuSkaicius) {
                 if (pazymiuKiekis[mIdx] == 0) {
                     cout << "Mokinys neturi pazymiu.\n";
                 } else {
                     cout << "Kuri pazymi keisti (1-" << pazymiuKiekis[mIdx] << ")? ";
                     cin >> pNr;
                     int pIdx = pNr - 1;
+                    
+                    // Jei pasirinko teisingą pažymio numerį
                     if (pIdx >= 0 && pIdx < pazymiuKiekis[mIdx]) {
                         int naujasPazymys;
+                        do {
+                            cout << "Iveskite nauja pazymi (1-10): ";
+                            cin >> naujasPazymys;
+                            if (naujasPazymys < 1 || naujasPazymys > 10) {
+                                cout << "Klaida! Pazymys turi buti nuo 1 iki 10.\n";
+                            }
+                        } while (naujasPazymys < 1 || naujasPazymys > 10);
+                        
+                        pazymiai[mIdx][pIdx] = naujasPazymys;
+                        cout << "Atnaujinta!\n";
+                    } else {
+                        cout << "Klaida: Neteisingas pazymio numeris.\n";
+                    }
+                }
+            } else {
+                cout << "Klaida: Tokio mokinio nera.\n";
+            }
+        }
+        // Mokinio trinimas
+        else if (pasirinkimas == 5) { 
+            int nr;
+            cout << "Iveskite mokinio numeri istrinimui: ";
+            cin >> nr;
+            int idx = nr - 1;
+            
+            // Jei mokinys yra
+            if (idx >= 0 && idx < mokiniuSkaicius) {
+                for (int i = idx; i < mokiniuSkaicius - 1; i++) {
+                    vardai[i] = vardai[i+1]; 
+                    pazymiuKiekis[i] = pazymiuKiekis[i+1]; 
+                    // Perkeliu ir visus jo pažymius iš 2d masyvo
+                    for (int j = 0; j < MAX_PAZYMIU; j++) {
+                        pazymiai[i][j] = pazymiai[i+1][j];
+                    }
+                }
+                // Kai mokiniai perstumti - sumažiname mokinių skaičiu
+                mokiniuSkaicius--;
+                cout << "Mokinys istrintas.\n";
+            } else {
+                cout << "Klaida: Tokio mokinio nera.\n";
+            }
+        }
+        else if (pasirinkimas != 0) {
+            cout << "Neatpazintas veiksmas. Bandykite dar karta.\n";
+        }
+    } while (pasirinkimas != 0); // Jei 0, ciklas baigiasi ir programa užsidaro
+
     return 0;
 }
